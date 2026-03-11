aec93c1bb2b2afb3c19efafa7fa5889a76a464b1
Anderson <andersonbteixeira@outlook.com>
2026-03-11 09:30:51 -0300

    update index

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,25 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-// https://vitejs.dev/config/
+
 export default defineConfig({
+    root: ".",
     build: {
         outDir: "build",
+        emptyOutDir: true,
         chunkSizeWarningLimit: 2000,
         rollupOptions: {
+            input: "./index.html",
             output: {
                 manualChunks: {
-                    vendor: ['react', 'react-dom', 'react-router-dom'],
-                    charts: ['recharts'],
-                    supabase: ['@supabase/supabase-js'],
+                    vendor:   ["react", "react-dom", "react-router-dom"],
+                    charts:   ["recharts"],
+                    supabase: ["@supabase/supabase-js"],
                 },
             },
         },
     },
     plugins: [tsconfigPaths(), react()],
     server: {
-        port: "4028",
+        port: 4028,
         host: "0.0.0.0",
         strictPort: true,
-    }
+    },
 });
